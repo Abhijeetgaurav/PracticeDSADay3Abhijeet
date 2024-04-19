5b5af19006f3ccfc2683f5a32eb99f3ffe7318a4
Abhijeet Gaurav <abhijeetgaurav800@gmail.com>
2024-04-19 15:11:25 +0530

    intial commit

---
+++ src/main/scala/BuyAndSellStock.scala
@@ -0,0 +1,25 @@
+class BuyAndSellStock {
+  def maxProfit(prices: Array[Int]): Int = {
+    def helper(prices: Array[Int], minPrice: Int, maxProfit: Int, idx: Int): Int = {
+      if (idx >= prices.length) maxProfit
+      else {
+        val currentPrice = prices(idx)
+        val newMinPrice = if (currentPrice < minPrice) currentPrice else minPrice
+        val newMaxProfit = if (currentPrice - newMinPrice > maxProfit) currentPrice - newMinPrice else maxProfit
+        helper(prices, newMinPrice, newMaxProfit, idx + 1)
+      }
+    }
+
+    if (prices.isEmpty) 0
+    else helper(prices, prices(0), 0, 1)
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val buyAndSellStock = new BuyAndSellStock()
+
+    val prices = Array(7, 1, 5, 3, 6, 4)
+    println("Maximum profit: " + buyAndSellStock.maxProfit(prices))
+  }
+}
